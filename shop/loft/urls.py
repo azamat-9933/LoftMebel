6cfa430bf5ef5fd24057516f11610524d7a292dd
azamat-9933 <azamat.yakhyayeff@gmail.com>
2022-07-15 18:58:00 +0500

    second

--- shop/loft/urls.py
+++ shop/loft/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import index, about
+from .views import index, about, contact, registration, favorites, basket, profile, product
 
 urlpatterns = [
     path('', index, name='index'),
-    path('about/', about, name='about')
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
+    path('registration/', registration, name='registration'),
+    path('favorites/', favorites, name='favorites'),
+    path('basket/', basket, name='basket'),
+    path('profile/', profile, name='profile'),
+    path('product/', product, name='product')
 ]
--- shop/loft/views.py
+++ shop/loft/views.py
@@ -7,3 +7,27 @@ def index(request):
 
 def about(request):
     return render(request, 'loft/about.html')
+
+
+def contact(request):
+    return render(request, 'loft/contact.html')
+
+
+def registration(request):
+    return render(request, 'loft/registration.html')
+
+
+def favorites(request):
+    return render(request, 'loft/favorites.html')
+
+
+def basket(request):
+    return render(request, 'loft/basket.html')
+
+
+def profile(request):
+    return render(request, 'loft/profile.html')
+
+
+def product(request):
+    return render(request, 'loft/product.html')
